2c9c81b9895173214293541f0a02f2baa925d02f
karasudidntwakeup <hemdanoovic@gmail.com>
2024-02-17 15:10:29 +0200

    first commit

---
+++ config.h
@@ -0,0 +1,22 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = { "Cascadia Code:size=12", "SF Arabic:size=11","JetBrains Mono Nerd Font:size=10"};
+
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#191919" },
+	[SchemeSel] = { "#eeeeee", "#ee5396" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
