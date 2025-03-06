a86d668d285670f40b1d19e6c97600aee92efcc8
karasudidntwakeup <hemdanoovic@gmail.com>
2025-03-06 17:05:29 +0200

    new font

--- config.h
+++ config.h
@@ -3,14 +3,14 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "Recursive Mono Casual Static:size=16", "SF Arabic:size=11","JetBrains Mono Nerd Font:size=10"};
+static const char *fonts[] = { "Comic Code:size=16", "SF Arabic:size=11","JetBrains Mono Nerd Font:size=10"};
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#191919" },
-	[SchemeSel] = { "#eeeeee", "#ee5396" },
-	[SchemeOut] = { "#191919", "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#ffffff" },
+	[SchemeSel] = { "#eeeeee", "#000000" },
+	[SchemeOut] = { "#ffffff", "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
